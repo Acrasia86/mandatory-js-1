28521938030f6629238f8b61cf921b3dbaf4342d
Jacob <Jacob.pahlsson@hotmail.com>
2019-12-16 14:13:29 +0100

    Changed to javascript

--- index.js
+++ index.js
@@ -19,29 +19,64 @@ var list = document.getElementById("contact");
 list.insertBefore(contact, list.childNodes[1]); 
 
 
+
+
+
+document.getElementById("about").parentNode.insertBefore(document.getElementById("about"), document.getElementById("contact"));
+
+
+var child = document.getElementsByTagName("td")[0];
+
+var parent = document.createElement("th");
+
+parent.textContent = "Name";
+
+child.replaceWith(parent);
+
+
+
+var child2 = document.getElementsByTagName("td")[0];
+
+var parent2 = document.createElement("th");
+
+parent2.textContent = "Email";
+
+child2.replaceWith(parent2);
+
+
 var about = document.createElement("h2");
 about.textContent = "About";
 var list = document.getElementById("about");  
 list.insertBefore(about, list.childNodes[0]); 
 
 
-document.getElementsByTagName("section")[0].style.position = "fixed";
+var about2 = document.createElement("p");
+about2.textContent = "We're the best in fruits & vegetables";
+var list2 = document.getElementById("about");  
+list.insertBefore(about2, list2.childNodes[1]); 
 
-document.getElementsByTagName("section")[0].style.bottom = "365px";
-
-
-document.getElementsByTagName("td")[0].style.position = "fixed";
-
-document.getElementsByTagName("td")[0].style.marginLeft = "20px"
-
-document.getElementsByTagName("td")[0].style.fontWeight = "bold";
+document.querySelector("#about").childNodes[2].remove();
 
 
+var callbackFunc = function(){
+    
+};
+var head = document.getElementsByTagName( "head" )[0];
+var fileref=document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", "main.css");
+
+    fileref.onload  = callbackFunc;
+    head.insertBefore( fileref, head.firstChild );
 
 
-document.getElementsByTagName("td")[1].style.fontWeight = "bold";
 
-document.getElementsByTagName("td")[1].style.paddingLeft = "40px"
+
+
+
+
+
 
 
 
